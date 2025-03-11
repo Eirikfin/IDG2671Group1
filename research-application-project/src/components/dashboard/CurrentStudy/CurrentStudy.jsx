9bc92bc1bf5ca4569059e5c19e1d31444f206e40
Falkebr <falkebr@stud.ntnu.no>
2025-03-11 01:40:30 +0100

    123

--- research-application-project/src/components/dashboard/CurrentStudy/CurrentStudy.jsx
+++ research-application-project/src/components/dashboard/CurrentStudy/CurrentStudy.jsx
@@ -5,7 +5,7 @@ export default function CurrentStudy() {
     return (
         <div className={styles.study}>
             <h2 className={styles.study_title}>Current study/studies</h2>
-            {[1, 2, 3].map((num) => (
+            {[1, 2, 3, 4].map((num) => (
                 <div key={num} className={styles.study_card}>
                     <h3>Study {num}: Compare rocks</h3>
                     <p>Number of participants: {Math.floor(Math.random() * 100)} (replace with dummy data later)</p>
