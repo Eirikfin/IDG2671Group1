a09c00ef67a9994e91331c0ed4a3c0fe9d884ed6
Eirikfin <148289492+Eirikfin@users.noreply.github.com>
2025-03-31 16:02:02 +0200

    planning artifact controllers

---
+++ back-end/controllers/artifacts.controllers.js
@@ -0,0 +1,12 @@
+import Artifact from "../models/artifacts.model";
+
+
+//storing uploaded artifact in db:
+
+//updating/replacing an artifact:
+
+//deleting an artifact:
+
+//getting an artifact by id:
+
+//getting all artifacts uploaded by a researcer:
--- back-end/middleware/webtoken.js
+++ back-end/middleware/webtoken.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import Researcher from "../models/researchers.model.js";
 
 dotenv.config();
@@ -7,27 +7,32 @@ dotenv.config();
 const secretKey = process.env.SECRET_KEY || "topsecretkey";
 
 
+//creating a token:
 export const createToken = (payload, secretKey) => {
-    
-        const token = jwt.sign(payload, secretKey, {expiresIn: "3d"});
-        return token
-}
+  const token = jwt.sign(payload, secretKey, { expiresIn: "3d" });
+  return token;
+};
 
-
+//authenticating a token:
 export const authenticateToken = (req, res, next) => {
-    try{
-        const token = req.headers.authorization?.split(" ")[1]; // getting the token from authorization (removing "Bearer ")
+  try {
+    const token = req.headers.authorization?.split(" ")[1]; // getting the token from authorization (removing "Bearer ")
 
-        if(!token){ //if no token was provided
-            return res.status(401).json({message: "Access denied. No token was provided."})
-        }
-        //decode the token
-        const decodedToken = jwt.verify(token, secretKey);
-        //set user to the payload of decoded token
-        req.user = decodedToken;
-
-        next(); //move on to next middleware
-    }catch(err){
-        return res.status(403).json({ message: "Token has expired, or is invalid." })
+    if (!token) {
+      //if no token was provided
+      return res
+        .status(401)
+        .json({ message: "Access denied. No token was provided." });
     }
-}
+    //decode the token
+    const decodedToken = jwt.verify(token, secretKey);
+    //set user to the payload of decoded token
+    req.user = decodedToken;
+
+    next(); //move on to next middleware
+  } catch (err) {
+    return res
+      .status(403)
+      .json({ message: "Token has expired, or is invalid." });
+  }
+};
