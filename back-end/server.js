1cad4e223a96433f96090373cbccd5071d8502ef
Falkebr <you@example.com>
2025-04-02 11:33:05 +0200

    unsure if /api/projects/questions/answers/:id is correct?

--- back-end/server.js
+++ back-end/server.js
@@ -21,6 +21,27 @@ app.set("views", "./views");
 app.get("/", (req, res) => {
     res.render("dashboard");
 })
+
+const answersRoute = require('./routes/answersRoute');
+const projectsRoute = require('./routes/projectsRoute');
+const questionsRoute = require('./routes/questionsRoute');
+
+app.use('/api/answers', answersRoute); // <-- remember to add authorization middleware etc.
+app.use('/api/projects', projectsRoute); // <-- remember to add authorization middleware etc.
+app.use('/api/questions', questionsRoute); // <-- remember to add authorization middleware etc.
+
+app.post('/api/projects') // <-- remember to add authorization middleware etc.
+app.patch('/api/projects/:id') // <-- remember to add authorization middleware etc.
+app.delete('/api/projects/:id') // <-- remember to add authorization middleware etc.
+
+app.post('/api/questions') // <-- remember to add authorization middleware etc.
+app.patch('/api/projects/questions/:id') // <-- remember to add authorization middleware etc.
+app.delete('/api/projects/questions/:id') // <-- remember to add authorization middleware etc.
+
+app.post('/api/answers') // <-- remember to add authorization middleware etc.
+app.patch('/api/projects/questions/answers/:id') // <-- remember to add authorization middleware etc.
+app.delete('/api/projects/questions/answers/:id') // <-- remember to add authorization middleware etc.
+
 // start server:
 app.listen(process.env.PORT, () => {
     console.log(`Server is listening at http://localhost:${process.env.PORT}`);
