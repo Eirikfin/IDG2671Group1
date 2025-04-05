a35355dff29bed5131aedf6ac55b3cd34dc5e465
Falkebr <falkebr@stud.ntnu.no>
2025-04-05 15:04:45 +0200

    implemented additional role-checking for CRUD operations associated with researchers in database

--- back-end/models/researchers.model.js
+++ back-end/models/researchers.model.js
@@ -4,6 +4,11 @@ const researcherSchema = new mongoose.Schema({
   name: { type: String, trim: true }, //researcher name, if needed
   email: { type: String, unique: true, required: true, trim: true }, //researcher email, will be used for researcher log in
   password: { type: String, required: true }, //password will be hashed/encrypted before storage
+  role: {
+    type: String,
+    enum: ['researcher', 'admin'],
+    default: 'researcher',
+  },
 });
 
 const Researcher = mongoose.model("Researchers", researcherSchema);
--- back-end/routes/researchers.route.js
+++ back-end/routes/researchers.route.js
@@ -2,6 +2,7 @@ import researcherValidator from "../validators/researcher.validator.js";
 import { createResearcher, updateResearcher, findResearcher, deleteResearcher } from "../controllers/researchers.controllers.js";
 import { passwordHash } from "../middleware/passwordhandling.js";
 import { authenticateToken } from "../middleware/webtoken.js";
+import { requireAdmin } from "../middleware/roleauth.js";
 import express from "express";
 
 const router = express.Router();
@@ -13,9 +14,9 @@ router.post("/", researcherValidator, passwordHash, createResearcher);
 router.put("/:id", researcherValidator, authenticateToken, updateResearcher);
 
 //deleting a researcher:
-router.delete("/:id", authenticateToken, deleteResearcher);
+router.delete("/:id", authenticateToken, requireAdmin, deleteResearcher);
 
 //find Researcher info:
-router.get("/:id", authenticateToken, findResearcher);
+router.get("/:id", authenticateToken, requireAdmin, findResearcher);
 
 export default router;
