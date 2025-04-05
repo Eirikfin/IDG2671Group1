19e346bb7417c25b514844ea33157391a2cb9f4e
Falkebr <falkebr@stud.ntnu.no>
2025-04-05 17:46:19 +0200

    generated css files for results components

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,12 +3,13 @@ import './assets/global-styles/App.css'
 import Header from './components/user-interface/header/ui_header';
 import Footer from './components/user-interface/Footer/ui_footer';
 import DashBoard from './dashboard/dashboard';
+import Results from './results/results';
 
 export default function App() {
   return(
     <>
       <Header></Header>
-      <DashBoard></DashBoard>
+      <Results></Results>
       <Footer></Footer>
     </>
   );
