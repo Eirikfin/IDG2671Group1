8fb8476564d24d3eb824c0ef4416e31bdad78fc0
Falkebr <falkebr@stud.ntnu.no>
2025-04-07 13:34:20 +0200

    added more react routes. still need to add more later on when all frontend components are finished

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/user-interface/header/ui_header';
 import Footer from './components/user-interface/Footer/ui_footer';
 import DashBoard from './dashboard/dashboard';
 import Results from './results/results';
+import Newstudy from './createsurveypage/newstudy';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function App() {
@@ -14,6 +15,7 @@ export default function App() {
 
         <Routes>
           <Route path="/" element={<DashBoard />} />
+          <Route path="/create_study" element={<Newstudy />} />
           <Route path="/results" element={<Results />} />
         </Routes>
         <Footer></Footer>
