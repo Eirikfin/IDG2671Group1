204aff1ec1e49d7b795e0c8bb472889158803292
Falkebr <falkebr@stud.ntnu.no>
2025-04-07 08:41:17 +0200

    same procedure as last commit, miss sophie

--- front-end/src/results/Answers/Answers.jsx
+++ front-end/src/results/Answers/Answers.jsx
@@ -1,7 +1,11 @@
+import styles from "./Answers.module.scss";
+
 export default function Answers() {
     return(
-        <div className="card">
-            <p>*GET 5 random answers from each question and display here*</p>
+        <div className={styles.container}>
+            <div className={styles.container_card}>
+                <p>*GET 5 random answers from each question and display here*</p>
+            </div>
         </div>
     );
 }
