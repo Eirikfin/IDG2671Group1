a6e55e80bb75cafe806a7fa5ea509d26ff5dc8be
Falkebr <falkebr@stud.ntnu.no>
2025-04-08 11:22:39 +0200

    testing endpoints and debugging (in progress)

--- back-end/routes/researchers.route.js
+++ back-end/routes/researchers.route.js
@@ -1,5 +1,5 @@
 import researcherValidator from "../validators/researcher.validator.js";
-import { createResearcher, updateResearcher, getResearcher, deleteResearcher } from "../controllers/researchers.controllers.js";
+import { createResearcher, updateResearcher, getResearcher, deleteResearcher, getAllResearchers } from "../controllers/researchers.controllers.js";
 import { passwordHash } from "../middleware/passwordhandling.js";
 import { authenticateToken } from "../middleware/webtoken.js";
 import { requireAdmin } from "../middleware/roleauth.js";
@@ -11,12 +11,15 @@ const router = express.Router();
 router.post("/", researcherValidator, passwordHash, createResearcher);
 
 //updating a researcher:
-router.put("/:id", researcherValidator, authenticateToken, updateResearcher);
+router.put("/:id", researcherValidator, passwordHash, authenticateToken, updateResearcher);
 
 //deleting a researcher:
 router.delete("/:id", authenticateToken, requireAdmin, deleteResearcher);
 
 //find Researcher info:
 router.get("/:id", authenticateToken, requireAdmin, getResearcher);
+
+//get all researchers:
+router.get("/", authenticateToken, requireAdmin, getAllResearchers);
 
 export default router;
