396f9c0d97855236619827751e9d6e1df4b23b93
Falkebr <falkebr@stud.ntnu.no>
2025-04-09 11:24:57 +0200

    added router.get for getAllProjects

--- back-end/routes/projects.route.js
+++ back-end/routes/projects.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { authenticateToken } from "../middleware/webtoken.js";
 import projectValidator from "../validators/projects.validator.js";
-import { createProject, updateProject, deleteProject, getProject } from "../controllers/project.controllers.js";
+import { createProject, updateProject, deleteProject, getProject, getAllProjects } from "../controllers/project.controllers.js";
 
 
 
@@ -18,5 +18,8 @@ router.delete("/:id", projectValidator, authenticateToken, deleteProject);
 
 //find project info:
 router.get("/:id", authenticateToken, getProject);
+
+//get all projects for a researcher:
+router.get("/researcher/:id", authenticateToken, getAllProjects);
 
 export default router;
