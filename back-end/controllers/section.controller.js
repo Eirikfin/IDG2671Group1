97bb6e5139632d27e9687f07b97d2fc42c966254
Eirikfin <148289492+Eirikfin@users.noreply.github.com>
2025-04-30 23:41:14 +0200

    added some new controllers

--- back-end/controllers/section.controller.js
+++ back-end/controllers/section.controller.js
@@ -37,14 +37,51 @@ try{
 
 
 export const getSection = async (req, res) => {
-    return res.send("Not implemented")
+    try{
+        const section = await QuestionSection.findById(req.params.id);
+
+        if(!section){
+            return res.status(404).json({message: "Project section not found"});
+        }
+
+        await section.populate('artifacts');
+
+        return res.status(200).json(section);
+
+
+    }catch(err){
+        return res.status(500).json({message: "Server error", error: err.message});
+    }
+
+
 }
 
 export const updateSection = async (req, res) => {
-    return
+ try{
+    const newSection = await QuestionSection.findByIdAndUpdate(req.params.id, req.body, {new: true});
+
+    if(!newSection){
+    return res.status(404).json({ message: "Question section was not found" });
+}
+    return res.status(200).json(newSection);
+
+}catch(err){
+    return res.status(500).json({ message: "Server error", error: err.message})
+}
 }
 
 export const deleteSection = async (req, res) => {
-    return
-}
+    try{
 
+        const section = await QuestionSection.findByIdAndDelete(req.params.id);
+
+        if(!section){
+            return res.status(404).json({message: "No section with this id was found"});
+        }
+        
+        return res.status(200).json({message: "Section was deleted sucessfully"});
+
+    }catch(err){
+        return res.status(500).json({message: "Server error", error: err.message});
+    }
+}
--- back-end/routes/sections.route.js
+++ back-end/routes/sections.route.js
@@ -1,11 +1,17 @@
 import express from "express";
 import { authenticateToken } from "../middleware/webtoken.js";
-import { createSection } from "../controllers/section.controller.js";
+import { createSection, getSection, updateSection, deleteSection } from "../controllers/section.controller.js";
 
 
 const router = express.Router();
 
+//post a new question section:
 router.post("/:projectId", authenticateToken, createSection);
-
+//get a question section:
+router.get("/:id", getSection);
+//update Question section:
+router.put("/:id", authenticateToken, updateSection);
+//delete a section:
+router.delete(":id", authenticateToken, deleteSection);
 
 export default router;
