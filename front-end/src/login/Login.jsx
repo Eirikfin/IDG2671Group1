8abef8e0f868f0ba592dc1cb7c7034e50deb652c
Falkebr <you@example.com>
2025-05-04 20:08:43 +0200

    juantothree

--- front-end/src/login/Login.jsx
+++ front-end/src/login/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
         setError('');
 
         try {
-            const response = await fetch(`${VITE_BASE_API_URL}/api/login`, {
+            const response = await fetch(`http://localhost:4202/api/login`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ email, password }),
