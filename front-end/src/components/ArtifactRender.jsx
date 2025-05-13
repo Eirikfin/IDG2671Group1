272bdf29dde5b47ea747e49862a95efd178c8184
Eirikfin <148289492+Eirikfin@users.noreply.github.com>
2025-05-13 14:14:27 +0200

    fixed image routes

--- front-end/src/components/ArtifactRender.jsx
+++ front-end/src/components/ArtifactRender.jsx
@@ -5,16 +5,16 @@ export default function ArtifactRender({artifact}) {
 
     if(artifact.mediaType === "image") {
         return <img 
-        src={`${apiUrl}/${artifact?.filepath}`} 
+        src={`${apiUrl}/api/${artifact?.filepath}`} 
         alt={artifact?.filename || `Artifact`}/>
     }
 
     if(artifact.mediaType === "audio") {
-        return <audio controls src={`${apiUrl}/${artifact?.filepath}`}></audio>
+        return <audio controls src={`${apiUrl}/api/${artifact?.filepath}`}></audio>
     }
 
     if(artifact.mediaType === "video") {
-        return <video controls src={`${apiUrl}/${artifact?.filepath}`}></video>
+        return <video controls src={`${apiUrl}/api/${artifact?.filepath}`}></video>
     }
 
         return <p>{`${apiUrl}/${artifact?.filepath}`}</p>
