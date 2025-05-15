5623b5960c6d4c453837d21489b92cd4366ffd8f
hannsam7 <hannsam@stud.ntnu.no>
2025-05-15 21:12:01 +0200

    New changes to website styling

--- front-end/src/components/user-interface/header/ui_header.jsx
+++ front-end/src/components/user-interface/header/ui_header.jsx
@@ -1,53 +1,75 @@
 import { useState, useEffect, useRef } from "react";
-import styles from './ui_header.module.scss';
-import { Link } from 'react-router-dom';
+import styles from "./ui_header.module.scss";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const menuRef = useRef(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef(null);
 
-    const menuDrop = () => {
-        setIsMenuOpen(!isMenuOpen);
+  const menuDrop = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  // Close menu when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        menuRef.current &&
+        !menuRef.current.contains(event.target) &&
+        !event.target.closest(`.${styles.menuButton}`)
+      ) {
+        setIsMenuOpen(false);
+      }
     };
 
-    // Close menu when clicking outside
-    useEffect(() => {
-        const handleClickOutside = (event) => {
-            if (
-                menuRef.current &&
-                !menuRef.current.contains(event.target) &&
-                !event.target.closest(`.${styles.menuButton}`)
-            ) {
-                setIsMenuOpen(false);
-            }
-        };
+    document.addEventListener("click", handleClickOutside);
+    return () => document.removeEventListener("click", handleClickOutside);
+  }, []);
 
-        document.addEventListener("click", handleClickOutside);
-        return () => document.removeEventListener("click", handleClickOutside);
-    }, []);
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.header_title}>Research Application</h1>
 
-    return (
-        <header className={styles.header}>
-            <h1 className={styles.header_title}>Research Application</h1>
-            
-            <nav>
-                <div>
-                    <button className={styles.menuButton} onClick={menuDrop}>
-                        Menu
-                    </button>
-                    <ul
-                        ref={menuRef}
-                        className={`${styles.menu} ${isMenuOpen ? styles.open : ""}`}
-                    >
-                        <li><Link to ="/">Profile</Link></li>
-                        <li><Link to="/">Manage database</Link></li>
-                        <li><Link to="/results">Results</Link></li>
-                    </ul>
-                </div>
-                <div>
-                    <button><Link to="/dashboard" className={styles.homeButton} >Home</Link></button>
-                </div>
-            </nav>
-        </header>
-    );
+    <div className={'logo'}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24px"
+        viewBox="0 -960 960 960"
+        width="24px"
+        fill="#e3e3e3"
+      >
+        <path d="M480-320q48 0 85.5-28.5T620-422H340q17 45 54.5 73.5T480-320ZM380-480q25 0 42.5-17.5T440-540q0-25-17.5-42.5T380-600q-25 0-42.5 17.5T320-540q0 25 17.5 42.5T380-480Zm200 0q25 0 42.5-17.5T640-540q0-25-17.5-42.5T580-600q-25 0-42.5 17.5T520-540q0 25 17.5 42.5T580-480ZM305-704l112-145q12-16 28.5-23.5T480-880q18 0 34.5 7.5T543-849l112 145 170 57q26 8 41 29.5t15 47.5q0 12-3.5 24T866-523L756-367l4 164q1 35-23 59t-56 24q-2 0-22-3l-179-50-179 50q-5 2-11 2.5t-11 .5q-32 0-56-24t-23-59l4-165L95-523q-8-11-11.5-23T80-570q0-25 14.5-46.5T135-647l170-57Zm49 69-194 64 124 179-4 191 200-55 200 56-4-192 124-177-194-66-126-165-126 165Zm126 135Z" />
+      </svg>
+      </div>
+
+      <nav>
+        <div>
+          <button className={styles.menuButton} onClick={menuDrop}>
+            Menu
+          </button>
+          <ul
+            ref={menuRef}
+            className={`${styles.menu} ${isMenuOpen ? styles.open : ""}`}
+          >
+            <li>
+              <Link to="/">Profile</Link>
+            </li>
+            <li>
+              <Link to="/">Manage database</Link>
+            </li>
+            <li>
+              <Link to="/results">Results</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <button>
+            <Link to="/dashboard" className={styles.homeButton}>
+              Home
+            </Link>
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
 }
